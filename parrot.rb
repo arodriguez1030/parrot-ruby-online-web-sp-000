b387c275c65d4b8e6529121bd9e06ac3e8b1c3b7
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-09 14:21:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string="Squawk!", *p)
- puts "#{string}" 
+ puts "#{string}"
+ "#{string}"
 end
 
 parrot("Pretty bird!")
